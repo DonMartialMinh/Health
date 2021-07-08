e8cc2cc65cc206f738051b8740595187937f8284
VoMinhDon <vominhdona1@gmail.com>
2021-07-08 08:45:31 +0700

    get bug on intro screen

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,8 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:health/views/tabs/Settings.dart';
 import 'package:health/views/tabs/WeightTracker.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import '../views/tabs/Foods.dart';
 import 'package:health/views/tabs/Fitness.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -140,3 +141,130 @@ class _HomePage extends State<HomePage> {
         ],
       );
 }
+
+// class OnBoardingPage extends StatefulWidget {
+//   @override
+//   _OnBoardingPageState createState() => _OnBoardingPageState();
+// }
+
+// class _OnBoardingPageState extends State<OnBoardingPage> {
+//   final introKey = GlobalKey<IntroductionScreenState>();
+//
+//   void _onIntroEnd(context) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute(builder: (context) => Scaffold(body: SignUpWidget(),)),
+//     );
+//   }
+//
+//   Widget _buildImage(String assetName, [double width = 350]) {
+//     return Image.asset('assets/images/$assetName', width: width);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     const bodyStyle = TextStyle(fontSize: 19.0);
+//
+//     const pageDecoration = const PageDecoration(
+//       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+//       bodyTextStyle: bodyStyle,
+//       descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+//       pageColor: Colors.white54,
+//       imagePadding: EdgeInsets.zero,
+//     );
+//
+//     return IntroductionScreen(
+//       key: introKey,
+//       globalBackgroundColor: Colors.white,
+//       globalHeader: Align(
+//         alignment: Alignment.topRight,
+//         child: SafeArea(
+//           child: Padding(
+//             padding: const EdgeInsets.only(top: 16, right: 16),
+//             child: _buildImage('appicon.png', 100),
+//           ),
+//         ),
+//       ),
+//       globalFooter: SizedBox(
+//         width: double.infinity,
+//         height: 60,
+//         child: ElevatedButton(
+//           child: const Text(
+//             'Let\s go right away!',
+//             style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+//           ),
+//           onPressed: () => _onIntroEnd(context),
+//         ),
+//       ),
+//       pages: [
+//         PageViewModel(
+//           title: "Fractional shares",
+//           body:
+//           "Instead of having to buy an entire share, invest any amount you want.",
+//           image: _buildImage('fitness.png'),
+//           decoration: pageDecoration,
+//         ),
+//         PageViewModel(
+//           title: "Learn as you go",
+//           body:
+//           "Download the Stockpile app and master the market with our mini-lesson.",
+//           image: _buildImage('food.jpeg'),
+//           decoration: pageDecoration,
+//         ),
+//         PageViewModel(
+//           title: "Kids and teens",
+//           body:
+//           "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+//           image: _buildImage('bmi.png'),
+//           decoration: pageDecoration,
+//         ),
+//         PageViewModel(
+//           title: "Title of last page - reversed",
+//           bodyWidget: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: const [
+//               Text("Click on ", style: bodyStyle),
+//               Icon(Icons.edit),
+//               Text(" to edit a post", style: bodyStyle),
+//             ],
+//           ),
+//           decoration: pageDecoration.copyWith(
+//             bodyFlex: 2,
+//             imageFlex: 4,
+//             bodyAlignment: Alignment.bottomCenter,
+//             imageAlignment: Alignment.topCenter,
+//           ),
+//           image: _buildImage('bmi.png'),
+//           reverse: true,
+//         ),
+//       ],
+//       onDone: () => _onIntroEnd(context),
+//       //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+//       showSkipButton: true,
+//       skipFlex: 0,
+//       nextFlex: 0,
+//       //rtl: true, // Display as right-to-left
+//       skip: const Text('Skip'),
+//       next: const Icon(Icons.arrow_forward),
+//       done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+//       curve: Curves.fastLinearToSlowEaseIn,
+//       controlsMargin: const EdgeInsets.all(16),
+//       controlsPadding: kIsWeb
+//           ? const EdgeInsets.all(12.0)
+//           : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+//       dotsDecorator: const DotsDecorator(
+//         size: Size(10.0, 10.0),
+//         color: Color(0xFFBDBDBD),
+//         activeSize: Size(22.0, 10.0),
+//         activeShape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(25.0)),
+//         ),
+//       ),
+//       dotsContainerDecorator: const ShapeDecoration(
+//         color: Colors.white54,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(8.0)),
+//         ),
+//       ),
+//     );
+//   }
+// }
